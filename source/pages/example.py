6eaa4d06c7706f7a4f4e223b4e560920344bb5b2
patrikastervall <patrik.astervall@gmail.com>
2022-08-18 11:06:11 +0200

    old updates

---
+++ source/pages/example.py
@@ -0,0 +1,3 @@
+class Thing():
+    def __init__():
+        self.x = 10
---
+++ source/pages/example_sql.sql
@@ -0,0 +1,15 @@
+CREATE PROCEDURE DoStuff
+AS
+
+-- Uses AdventureWorksDW
+DECLARE @maxWeight FLOAT, @productKey INTEGER
+SET @maxWeight = 100.00
+SET @productKey = 424
+IF @maxWeight <= (SELECT Weight from DimProduct WHERE ProductKey = @productKey)
+    SELECT @productKey AS ProductKey, EnglishDescription, Weight, 'This product is too heavy to ship and is only available for pickup.'
+        AS ShippingStatus
+    FROM DimProduct WHERE ProductKey = @productKey
+ELSE
+    SELECT @productKey AS ProductKey, EnglishDescription, Weight, 'This product is available for shipping or pickup.'
+        AS ShippingStatus
+    FROM DimProduct WHERE ProductKey = @productKey
